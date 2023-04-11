7bc40d61e2100ebf7addfac34abecc51225d39c8
gigigabytes <106554500+gigigabytes@users.noreply.github.com>
2023-04-11 18:26:25 +0000

    Questão D do codeforces

---
+++ Codeforces/questaoD.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int h(int a, int b){
+  int s;
+  s = a + b;
+  return 0;
+}
+
+int mult() {
+  int total = a*60+b*60;
+  return 0;
+}
+
+int main() {
+  int a, b;
+  printf("%d\n", mult(a, b));
+ );
+scanf("%d%*c%d%", &hora, &min;
+  return 0;
+}
